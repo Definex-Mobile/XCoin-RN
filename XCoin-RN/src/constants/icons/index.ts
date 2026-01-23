e5e4050200e018293ff084f5119680eaf21bbba0
efenlbnt <efe.nalbant@teamdefinex.com>
2026-01-23 14:40:13 +0300

    folders update for svg icons

---
+++ XCoin-RN/src/constants/icons/index.ts
@@ -0,0 +1,6 @@
+export { ic_history } from "./history";
+export { ic_bank_details } from "./bankDetails";
+export { ic_notifications } from "./notifications";
+export { ic_security } from "./security";
+export { ic_help_support } from "./helpSupport";
+export { ic_terms_conditions } from "./termsConditions";
